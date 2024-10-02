b37bd8c3f0c78f461a9faa46e7be4f3a3c282837
Thomas <thomas@likesistemas.com.br>
2024-10-02 20:03:31 -0300

    construido um array

---
+++ Versionamento/exercicio2.js
@@ -0,0 +1,5 @@
+const furtas = ['maca','manga','morango','morango','melancia','maracuja']
+
+console.log("A fruta na posição 1 é: ", furtas[1]);
+console.log("A fruta na posição 1 é: ", furtas[4]);
+console.log("meu array tem ", furtas.length, " furas");
