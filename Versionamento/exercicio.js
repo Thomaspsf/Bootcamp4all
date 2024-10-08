f62ad72c58909ebcfa492f0d60bdad46e778a72f
Thomas <thomas@likesistemas.com.br>
2024-10-07 22:32:30 -0300

    adicionado novos comando do git

--- Versionamento/exercicio.js
+++ Versionamento/exercicio.js
@@ -6,11 +6,12 @@ function some(num1, num2) {
 }
 
 some(10,20);
-
+// git clone URL
 // git config user.email "thomas_psf@hotmail.com"
 // git config user.name "Thomaspsf"
-// git add .
-// git commit -m "descricao do seu codigo"
-// git push origin main
+// git add . -- cria o pacote de commit
+// git commit -m "descricao do seu codigo"  -- fecha o pacote de commit
+// git push origin main -- envia os commit para o git
+// git pull -- trazer local as alteraçoes
 // git checkout nomebranch -- trocar de branch que ja existe
 // git checkout -b nomebranch -- cria uma nova branch
